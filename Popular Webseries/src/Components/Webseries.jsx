a459f1c7ee5da0ab0a5af82a13120e23ceaed623
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-05-21 16:46:52 +0530

    delete all css and html again write a code from scrach

--- Popular Webseries/src/Components/Webseries.jsx
+++ Popular Webseries/src/Components/Webseries.jsx
@@ -1,24 +1,10 @@
 import React from "react";
 
+
 const Webseries = (props) => {
   return (
     <>
-      <h2 className="heading">
-        Hotstar <span className="Head-span">Specials</span>
-      </h2>
-
-      <div className="cards">
-        <div className="card">
-          <img src={props.imgsrc} alt="" className="img_card" />
-          <div className="card_info">
-            <span className="card_category">{props.title}</span>
-            <h3 className="card_title">{props.sname}</h3>
-            <a href={props.link}>
-              <button className="btn">Watch Now</button>
-            </a>
-          </div>
-        </div>
-      </div>
+     
     </>
   );
 };
