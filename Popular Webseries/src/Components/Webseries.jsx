bd7871c4d0cb8be1ddc3de6fe3ca122b95ffe4a9
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-05-21 15:21:00 +0530

    add heading and its style and html body structure

--- Popular Webseries/src/Components/Webseries.jsx
+++ Popular Webseries/src/Components/Webseries.jsx
@@ -3,7 +3,20 @@ import React from "react";
 const Webseries = () => {
   return (
     <>
-      <h2 className="">Popular Webseries</h2>
+      <h2 className="heading">Hotstar <span className="Head-span">Specials</span></h2>
+
+      <div className="cards">
+        <div className="card">
+          <img src="" alt="" className="img_card" />
+          <div className="card_info">
+            <span className="card_category"></span>
+            <h3 className="card_title"></h3>
+            <a href="">
+              <button>Watch Now</button>
+            </a>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
