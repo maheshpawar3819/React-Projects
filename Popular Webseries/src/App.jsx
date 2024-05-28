4634d4749c34c75b50fc3d02c44d688149b840e4
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-05-28 18:33:35 +0530

    clean all code form popular webseries project the next prop project will be popular webseries

--- Popular Webseries/src/App.jsx
+++ Popular Webseries/src/App.jsx
@@ -9,12 +9,7 @@ function App() {
 
   return (
     <>
-      <Webseries
-        imgsrc="https://img10.hotstar.com/image/upload/f_auto/sources/r1/cms/prod/1677/1581677-t-81f48f570f37"
-        title="A Hotstar Specials Series"
-        sname="Special Ops"
-        link="https://www.hotstar.com/in/shows/special-ops/1260022894/watch?fallbackEpisodeId=1260022902"
-      />
+      <Webseries/>
       
     </>
   );
