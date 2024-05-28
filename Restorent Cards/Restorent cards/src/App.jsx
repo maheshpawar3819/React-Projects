2ad8b9b7c523442f095b254066b03c2354534c06
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-05-28 10:48:13 +0530

    add body

--- Restorent Cards/Restorent cards/src/App.jsx
+++ Restorent Cards/Restorent cards/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './Components/Header'
+import Body from './Components/Body'
 
 /*
 Plan:
@@ -27,6 +28,7 @@ function App() {
   return (
     <>
       <Header/>
+      <Body/>
     </>
   )
 }
