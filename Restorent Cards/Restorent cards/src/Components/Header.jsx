6fe9cb2a1de9f25889adb78e42e326f16f4a3b5f
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-06-01 15:23:13 +0530

    fixed problem

--- Restorent Cards/Restorent cards/src/Components/Header.jsx
+++ Restorent Cards/Restorent cards/src/Components/Header.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-
+import { LOGO_URL } from "./Utlities/logourl";
 const Header = () => {
   return (
     <div className="header">
       <div className="logo-container">
         <img
-          src="https://img.freepik.com/free-vector/free-delivery-logo-with-bike-man-courier_1308-46261.jpg?size=626&ext=jpg&ga=GA1.1.253643294.1694699196&semt=ais_user_b"
+          src={LOGO_URL}
           alt="logo img"
           className="logo"
         />
---
+++ Restorent Cards/Restorent cards/src/Components/Utlities/logourl.js
@@ -0,0 +1,2 @@
+export const LOGO_URL =
+  "https://img.freepik.com/free-vector/free-delivery-logo-with-bike-man-courier_1308-46261.jpg?size=626&ext=jpg&ga=GA1.1.253643294.1694699196&semt=ais_user_b";
