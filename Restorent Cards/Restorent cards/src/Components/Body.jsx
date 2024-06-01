726ca10df4389b953d3082e8c0c121b1fc15cb85
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-06-01 17:59:43 +0530

    add filter button

--- Restorent Cards/Restorent cards/src/Components/Body.jsx
+++ Restorent Cards/Restorent cards/src/Components/Body.jsx
@@ -1,11 +1,24 @@
-import React from "react";
 import Restorentcard from "./Restorentcard";
+import { useState } from "react";
 import data from "./Utlities/Apidata";
 const Body = () => {
+  const [restaurent, setrestaurent] = useState(data);
+
   return (
     <div className="body">
+      <div>
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filterdata = restaurent.filter((rest) => rest.Ratings >=4);
+            setrestaurent(filterdata);
+          }}
+        >
+          Top Rated Restorents
+        </button>
+      </div>
       <div className="rest-container">
-        {data.map((restaurent, index) => {
+        {restaurent.map((restaurent, index) => {
           return <Restorentcard key={index} restaurent={restaurent} />;
         })}
       </div>
