1f95dc0b17bbb9bb377ff73c6b8cf9caaaadc55a
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-06-02 12:27:14 +0530

    modify readme.md file

--- Popular Webseries/src/Components/Webseries.jsx
+++ Popular Webseries/src/Components/Webseries.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const Webseries = (props) => {
   return (
     <>
-     
+     <p className="text-sky-400/100 bg-slate-800	">The quick brown fox...</p>
     </>
   );
 };
