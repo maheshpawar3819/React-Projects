7775dfe65c49493d7d3c1e482ea3489fdda738b1
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-06-03 22:16:16 +0530

    moving the serach bar header to body

--- Restorent Cards/Restorent cards/src/Components/Body.jsx
+++ Restorent Cards/Restorent cards/src/Components/Body.jsx
@@ -13,7 +13,7 @@ const Body = () => {
     <h1>Loading</h1>
   ) : (
     <div className="body">
-      <div>
+      <div className="search-btns">
         <button
           className="filter-btn"
           onClick={() => {
@@ -23,7 +23,22 @@ const Body = () => {
         >
           Top Rated Restorents
         </button>
+        <div className="search-container">
+        <input type="text" placeholder="Search" name="search" />
+        <button className="serach-btn">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            viewBox="0 0 16 16"
+          >
+            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+          </svg>
+        </button>
       </div>
+      </div>
+      
       <div className="rest-container">
         {restaurent.map((restaurent, index) => {
           return <Restorentcard key={index} restaurent={restaurent} />;
--- Restorent Cards/Restorent cards/src/Components/Header.jsx
+++ Restorent Cards/Restorent cards/src/Components/Header.jsx
@@ -2,28 +2,14 @@ import React from "react";
 import { LOGO_URL } from "./Utlities/logourl";
 import { useState } from "react";
 const Header = () => {
-
-    const [loginbtn,setloginbtn]=useState("Login");
+  const [loginbtn, setloginbtn] = useState("Login");
 
   return (
     <div className="header">
       <div className="logo-container">
         <img src={LOGO_URL} alt="logo img" className="logo" />
       </div>
-      <div className="search-container">
-        <input type="text" placeholder="Search" name="search" />
-        <button className="serach-btn">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            viewBox="0 0 16 16"
-          >
-            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
-          </svg>
-        </button>
-      </div>
+
       <div className="nav-items">
         <ul>
           <li>Home</li>
@@ -33,9 +19,14 @@ const Header = () => {
         </ul>
       </div>
       <div className="btn-div">
-        <button className="login-btn" onClick={() => {
-          loginbtn==="login" ? setloginbtn("logout"):setloginbtn("login");
-        }}>{loginbtn}</button>
+        <button
+          className="login-btn"
+          onClick={() => {
+            loginbtn === "login" ? setloginbtn("logout") : setloginbtn("login");
+          }}
+        >
+          {loginbtn}
+        </button>
       </div>
     </div>
   );
