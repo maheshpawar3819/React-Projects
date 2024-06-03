cc2fb565db08750aa1990855ba6470036c7f456c
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-06-03 23:03:35 +0530

    add the search and create full oprational search bar

--- Restorent Cards/Restorent cards/src/App.jsx
+++ Restorent Cards/Restorent cards/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
     <>
       <Header />
       <Body />
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 }
--- Restorent Cards/Restorent cards/src/Components/Body.jsx
+++ Restorent Cards/Restorent cards/src/Components/Body.jsx
@@ -3,14 +3,16 @@ import { useState } from "react";
 import data from "./Utlities/Apidata";
 const Body = () => {
   const [restaurent, setrestaurent] = useState(data);
+  const [serach, setSearch] = useState("");
 
+  console.log("body rander");
   // Conditional Radering
   // if(restaurent.length===0){
   //   return <h1>Loading...</h1>
   // }
 
   return restaurent.length === 0 ? (
-    <h1>Loading</h1>
+    <h1>Loading....</h1>
   ) : (
     <div className="body">
       <div className="search-btns">
@@ -23,22 +25,42 @@ const Body = () => {
         >
           Top Rated Restorents
         </button>
-        <div className="search-container">
-        <input type="text" placeholder="Search" name="search" />
-        <button className="serach-btn">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            viewBox="0 0 16 16"
+        <div
+          className="search-container"
+          value={serach}
+          onChange={(text) => {
+            setSearch(text.target.value);
+          }}
+        >
+          <input
+            type="text"
+            placeholder="Search"
+            name="search"
+            className="input1"
+          />
+          <button
+            className="serach-btn"
+            onClick={() => {
+              const result = restaurent.filter((rest) => {
+               return rest.rtname.toLowerCase().includes(serach.toLowerCase());
+              });
+
+              setrestaurent(result);
+            }}
           >
-            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
-          </svg>
-        </button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              viewBox="0 0 16 16"
+            >
+              <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+            </svg>
+          </button>
+        </div>
       </div>
-      </div>
-      
+
       <div className="rest-container">
         {restaurent.map((restaurent, index) => {
           return <Restorentcard key={index} restaurent={restaurent} />;
