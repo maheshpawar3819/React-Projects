35076e3b9414dd059f56d297e106f7e0db12b60d
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-06-06 20:14:23 +0530

    solve page randering issue

--- Restorent Cards/Restorent cards/src/App.jsx
+++ Restorent Cards/Restorent cards/src/App.jsx
@@ -1,9 +1,7 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 import Header from "./Components/Header";
 import Body from "./Components/Body";
-import Footer from "./Components/Footer";
 
 
 function App() {
--- Restorent Cards/Restorent cards/src/Components/Header.jsx
+++ Restorent Cards/Restorent cards/src/Components/Header.jsx
@@ -13,8 +13,8 @@ const Header = () => {
       <div className="nav-items">
         <ul>
           <li>Home</li>
+          <li>Menu</li>
           <li>About Us</li>
-          <li>Contact Us</li>
           <li>Cart</li>
         </ul>
       </div>
