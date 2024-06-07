41dd46b08a3c928c76ccffa522c4c4b2652fe81e
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-06-07 14:49:57 +0530

    add error.jsx file

--- Restorent Cards/Restorent cards/src/Components/Error.jsx
+++ Restorent Cards/Restorent cards/src/Components/Error.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import { useRouteError } from "react-router-dom";
 
 const Error = () => {
-    const err = useRouteError();
-console.log(err)
+  const err = useRouteError();
+  console.log(err);
   return (
     <div>
       <h1>Oops Page not Found!!</h1>
       <h2>Something went wrong!!!!!</h2>
-      <h3 style={{color:"red"}}>{err.status}:{err.statusText}</h3>
+      <h3 style={{ color: "red" }}>
+        {err.status}:{err.statusText}
+      </h3>
     </div>
   );
 };
