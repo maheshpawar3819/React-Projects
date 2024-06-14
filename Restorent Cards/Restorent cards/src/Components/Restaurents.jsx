8505b6bef714a288b853a9a77edaee5b44e66d50
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-06-14 16:32:36 +0530

    add all rest btn

--- Restorent Cards/Restorent cards/src/Components/Restaurents.jsx
+++ Restorent Cards/Restorent cards/src/Components/Restaurents.jsx
@@ -1,64 +1,80 @@
 import React from "react";
 import { useEffect, useState } from "react";
-import axios from "axios";
+import axios, { all } from "axios";
 import Cards from "./Cards/Cards";
 const Restaurents = () => {
   const [restaurentlist, setrestaurentlist] = useState([]);
-  
+  const [allrest, setAllrest] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [serach, setSearch] = useState("");
 
-  const fetchrestdata = () => {
-    axios
-      .get(
+  const fetchrestdata = async () => {
+    try {
+      const fetch = await axios.get(
         "https://www.swiggy.com/mapi/homepage/getCards?lat=18.61610&lng=73.72860"
-      )
-      .then((response) => {
-        console.log(response);
-        setrestaurentlist(
-          response?.data?.data?.success?.cards[1]?.gridWidget?.gridElements
-            ?.infoWithStyle?.restaurants
-        );
-        console.log(
-          response?.data?.data?.success?.cards[1]?.gridWidget?.gridElements
-            ?.infoWithStyle?.restaurants
-        );
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+      );
+      const restaurents =
+        fetch?.data?.data?.success?.cards[1]?.gridWidget?.gridElements
+          ?.infoWithStyle?.restaurants;
+      setrestaurentlist(restaurents || []);
+      setAllrest(restaurents || []);
+      setLoading(false);
+    } catch (error) {
+      console.log("Error is found", error);
+      setLoading(false);
+    }
   };
 
   useEffect(() => {
     fetchrestdata();
   }, []);
 
-  const filterrest=() => {
-    const filtd=restaurentlist.filter((rest) => {
-      return rest.info.avgRating>=4;
-    })
-    console.log(filtd)
+  const filterrest = () => {
+    const filtd = restaurentlist.filter((rest) => {
+      return rest.info.avgRating >= 4;
+    });
+    console.log(filtd);
     setrestaurentlist(filtd);
-  }
+  };
 
+  const showallrest = () => {
+    setrestaurentlist(allrest);
+  };
 
-  return restaurentlist === 0 ? (
+  return restaurentlist.length === 0 ? (
     <h1>Loading..</h1>
   ) : (
     <div className="restaurents">
       <div className="header-content">
-        <button
-          className="r-top-restbtn"
-          onClick={filterrest}
-        >
+        <button className="r-top-restbtn" onClick={filterrest}>
           <i className="bi bi-filter"></i>Top Rated Restaurents
         </button>
         <div className="r-search-container">
-          <input type="text" placeholder="Search..." className="r-input" />
-          <button className="r-serach-btn">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="r-input"
+            value={serach}
+            onChange={(event) => {
+              setSearch(event.target.value);
+            }}
+          />
+          <button
+            className="r-serach-btn"
+            onClick={() => {
+              const filterrest = restaurentlist.filter((rest) => {
+                return rest.name.toLowerCase().includes(serach.toLowerCase());
+              });
+              setrestaurentlist(filterrest);
+            }}
+          >
             <i className="bi bi-search"></i>
           </button>
         </div>
         <div className="r-allrest-div">
-          <button className="r-allrest-btn" >All Retaurents</button>
+          <button className="r-allrest-btn" onClick={showallrest}>
+            All Retaurents
+          </button>
         </div>
       </div>
       <div className="rest-container">
