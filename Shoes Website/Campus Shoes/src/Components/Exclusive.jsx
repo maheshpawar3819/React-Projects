c8986e0d295f0028396e3985f4c7c2f80ab9962f
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-06-19 20:21:20 +0530

    final the structure of the exclusive component only css styling is remaning

--- Shoes Website/Campus Shoes/src/Components/Exclusive.jsx
+++ Shoes Website/Campus Shoes/src/Components/Exclusive.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
-const Exclusive = ({exclusive}) => {
-  const {  urlName ,images } = exclusive || {};
-  const img=images[0]?.url;
-  console.log(img)
+const Exclusive = ({ exclusive }) => {
+  const { urlName, images, variants, options } = exclusive || {};
+  const img = images[0]?.url;
+  const price = variants[0]?.price;
+  const option = options[1]?.values;
+
   return (
     <div>
       <div>
@@ -14,6 +16,12 @@ const Exclusive = ({exclusive}) => {
       </div>
       <div>
         <h4>{urlName}</h4>
+      </div>
+      <div>
+        <h6>Rs. {price}</h6>
+      </div>
+      <div>
+        <p>{option.join(" , ")}</p>
       </div>
     </div>
   );
