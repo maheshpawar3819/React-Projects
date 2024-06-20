b7c43941b8e250b9a509c5c71ae57fde732c5f30
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-06-20 11:11:02 +0530

    final the styling

--- Shoes Website/Campus Shoes/src/Components/Exclusive.jsx
+++ Shoes Website/Campus Shoes/src/Components/Exclusive.jsx
@@ -1,27 +1,29 @@
 import React from "react";
 
 const Exclusive = ({ exclusive }) => {
-  const { urlName, images, variants, options } = exclusive || {};
+  const { title, images, variants, options } = exclusive || {};
   const img = images[0]?.url;
   const price = variants[0]?.price;
   const option = options[1]?.values;
 
   return (
-    <div>
+    <div className="border-solid border-2 border-black h-auto w-72 p-1">
       <div>
-        <img src={img} alt="" />
+        <img src={img} alt="" className="size-80" />
+      </div>
+      <div className="flex justify-center my-2 w-[100%] h-11">
+        <button className="text-white hover:text-black hover:border-solid hover:border-2 hover:border-gray-200 hover:bg-white p-2 px-16 tracking-widest rounded-sm text-xl">
+          QUICK BUY
+        </button>
       </div>
       <div>
-        <button>Quick Buy</button>
+        <p className="text-base text-gray-500 font-medium">{title}</p>
+      </div>
+      <div className="my-1">
+        <p className="text-xl ">Rs. {price}</p>
       </div>
       <div>
-        <h4>{urlName}</h4>
-      </div>
-      <div>
-        <h6>Rs. {price}</h6>
-      </div>
-      <div>
-        <p>{option.join(" , ")}</p>
+        <p className="text-base text-gray-500">{option.join("   , ")}</p>
       </div>
     </div>
   );
