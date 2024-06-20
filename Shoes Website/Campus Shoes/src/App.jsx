be2ce14822d9a37cce02f298b8aa78bf18cec4a3
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-06-20 14:42:46 +0530

    create shimmer ui component

--- Shoes Website/Campus Shoes/src/App.jsx
+++ Shoes Website/Campus Shoes/src/App.jsx
@@ -1,24 +1,28 @@
-import { useState } from 'react'
-import Header from './Components/Header'
-import './App.css'
-import Body from './Components/Body'
-import Mens from './Components/Mens'
-import Womens from './Components/Womens'
-import Kids from './Components/Kids'
-import Sandals from './Components/Sandals'
+import { useState } from "react";
+import Header from "./Components/Header";
+import "./App.css";
+import Body from "./Components/Body";
+import Mens from "./Components/Mens";
+import Womens from "./Components/Womens";
+import Kids from "./Components/Kids";
+import Sandals from "./Components/Sandals";
+import Shimmer from "./Components/Shimmer";
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    <Header/> 
-    {/* <Body/> */}
-    {/* <Mens/> */}
-    {/* <Womens/> */}
-    {/* <Kids/> */}
-    <Sandals/>
+      <Header />
+      {/* <Body /> */}
+
+      {/* 
+    <Mens/>
+    <Womens/>
+    <Kids/>
+    <Sandals/> */}
+      <Shimmer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ Shoes Website/Campus Shoes/src/Components/Shimmer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Shimmer = () => {
+  return (
+    <div className="box-border mt-28">
+      <h1>hii mahi</h1>
+    </div>
+  );
+};
+
+export default Shimmer;
