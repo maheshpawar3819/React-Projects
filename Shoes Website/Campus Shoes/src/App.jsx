0f68b185a016f33ec4f13681581d3fec72bc6034
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-06-23 20:54:56 +0530

    modify

--- Shoes Website/Campus Shoes/src/App.jsx
+++ Shoes Website/Campus Shoes/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Header from "./Components/Header";
 import "./App.css";
 import { Outlet } from "react-router-dom";
-import Shimmer from "./Components/Shimmer";
+
 function App() {
   const [count, setCount] = useState(0);
 
