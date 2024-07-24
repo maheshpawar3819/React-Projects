65097a2f7cce19165c471392c62a821634e4c746
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-07-24 15:57:59 +0530

    add foote

--- Shoes Website/Campus Shoes/src/App.jsx
+++ Shoes Website/Campus Shoes/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 import "./App.css";
 import { Outlet } from "react-router-dom";
 
@@ -10,6 +11,7 @@ function App() {
     <>
       <Header />
       <Outlet/>
+      <Footer/>
       {/* <Body /> */}
 
       {/* 
