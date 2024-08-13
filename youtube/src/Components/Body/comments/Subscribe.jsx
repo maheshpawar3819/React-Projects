6267bb30b5db485da432bb399af75de0c014acc4
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-08-13 15:28:28 +0530

    Complete Subscribe component

--- youtube/src/Components/Body/comments/Subscribe.jsx
+++ youtube/src/Components/Body/comments/Subscribe.jsx
@@ -2,6 +2,13 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { useSearchParams } from "react-router-dom";
 import useAddvideos from "../../../Utils/Hooks/useAddvideos";
+import {
+  likelogo,
+  dislike,
+  sharelogo,
+  downloadlogo,
+  savelogo,
+} from "../../../Utils/logos";
 
 const Subscribe = () => {
   const channeldata = useSelector((store) => store.video.videos);
@@ -9,25 +16,39 @@ const Subscribe = () => {
   useAddvideos();
 
   if (!channeldata || channeldata.length === 0) {
-    return <p>loading...</p>;
+    return <p className="pl-5 py-1">loading...</p>;
   }
 
-  // console.log(searchParams.get("v"));
   const channelId = searchParams.get("v");
   const channel = channeldata.find((ch) => ch.id === channelId);
-  // console.log(channel?.snippet?.channelTitle);
-  // console.log(channel?.snippet?.localized?.title);
   const { title } = channel?.snippet?.localized || "title is not availabe";
   const { channelTitle } = channel?.snippet;
 
   return (
     <>
-      <p className="pl-5 py-2">{title}</p>
-      <div className="pl-5 py-2 border-2 ">
+      <p className="pl-5 py-1">{title}</p>
+      <div className="pl-5 py-1 flex ">
         <div className="flex">
           <p>{channelTitle}</p>
-          <button className="ml-3 bg-red-700 rounded-2xl px-2 p-1 text-white">
+          <button className="ml-3 bg-red-700 rounded-2xl px-2 p-1 text-white hover:bg-red-500">
             Subscribe
+          </button>
+        </div>
+        <div className="ml-[500px]">
+          <button className="bg-gray-300 px-5 py-2 rounded-l-2xl hover:bg-white ml-3">
+            {likelogo}
+          </button>
+          <button className="bg-gray-300 px-4 py-2 rounded-r-2xl border-l-2 border-gray-400 hover:bg-red-600">
+            {dislike}
+          </button>
+          <button className="bg-gray-300 px-8  ml-3 py-2 rounded-2xl hover:bg-gray-500">
+            {sharelogo}
+          </button>
+          <button className="bg-gray-300 px-8  ml-3 py-2 rounded-2xl hover:bg-gray-500">
+            {downloadlogo}
+          </button>
+          <button className="bg-gray-300 px-8  ml-3 py-2 rounded-2xl hover:bg-gray-500">
+            {savelogo}
           </button>
         </div>
       </div>
